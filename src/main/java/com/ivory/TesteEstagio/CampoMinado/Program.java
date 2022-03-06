eb7294062d491b86f5e62921f94769f34fef3ab8
musquitinh0 <filipe.silva@ccc.ufcg.edu.br>
2022-03-06 01:25:22 -0300

    Adiciona função para converter a entrada em string do tabuleiro para uma matriz de caracteres

--- src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
+++ src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
@@ -1,5 +1,6 @@
 package com.ivory.TesteEstagio.CampoMinado;
 
+
 public class Program {
 	
 	public void executar() {
@@ -10,6 +11,26 @@ public class Program {
 		System.out.println(campoMinado.Tabuleiro());
 		
 		// Realize sua codificação a partir deste ponto, boa sorte!
+
+		
+		char[][] tabuleiroMat = new char[SIZE][SIZE];
+		while(campoMinado.JogoStatus().equals(StatusTipo.Aberto)){
+			tabuleiroMat =  matrixTabuleiro(campoMinado.Tabuleiro());
+
+			break;
+		}
 	}
 
+	private static final int SIZE = 9;
+
+
+	public char[][] matrixTabuleiro(String tab){
+		char[][] tabuleiroMat = new char[SIZE][SIZE];
+		for(int i = 0; i < SIZE; i ++){
+			for(int j = 0; j < SIZE; j++){
+				tabuleiroMat[i][j] = tab.charAt((SIZE+2)*i + j);
+			}
+		}
+		return tabuleiroMat;
+	}
 }
