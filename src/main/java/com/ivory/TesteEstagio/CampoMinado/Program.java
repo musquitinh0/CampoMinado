a33f4d9331b7545916f46d654c1e3efac5421805
musquitinh0 <filipe.silva@ccc.ufcg.edu.br>
2022-03-06 18:53:08 -0300

    implementado o jogador de campo minado

--- src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
+++ src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
@@ -9,18 +9,83 @@ public class Program {
 		
 		System.out.println("Início do jogo\n=======");
 		System.out.println(campoMinado.Tabuleiro());
+
 		
 		// Realize sua codificação a partir deste ponto, boa sorte!
-
+		boolean done = true;
+		
 		char[][] tabuleiroMat = new char[SIZE][SIZE];
-		tabuleiroMat =  matrizTabuleiro(campoMinado.Tabuleiro());//Tabuleiro auxiliar no formato de matriz e com as provaveis localizações das bombas dadas por 'p'(pra ficar parecido com a bandeira do jogo de verdade)
-		System.out.println(contaAbertas(4,7,tabuleiroMat));
-		while(campoMinado.JogoStatus().equals(StatusTipo.Aberto)){
-			break;
-		}
-	}
 
-	private static final int SIZE = 9;
+		while(campoMinado.JogoStatus().ordinal() == 0 && done){
+			//variavel de controle para saber se o algoritimo fez alguma coisa nessa iteração
+			done = false;
+
+			tabuleiroMat =  matrizTabuleiro(campoMinado.Tabuleiro());
+			tabuleiroMat = colocaBombas(tabuleiroMat);
+	
+			for(int linha = 0; linha < SIZE; linha++){
+				for(int coluna = 0; coluna < SIZE; coluna++){
+					if(tabuleiroMat[linha][coluna] != 'p' && tabuleiroMat[linha][coluna] != '-'){
+						if(contaBombas(linha, coluna, tabuleiroMat) == Character.getNumericValue(tabuleiroMat[linha][coluna])){
+							//todas as casas ao redor dessa peça sao seguras
+							if(linha > 0 && coluna > 0){
+								if(tabuleiroMat[linha-1][coluna-1] == '-'){
+									campoMinado.Abrir(linha, coluna);
+									done = true;
+								}
+							}
+							if(linha < SIZE-1 && coluna > 0){
+								if(tabuleiroMat[linha+1][coluna-1] == '-'){
+									campoMinado.Abrir(linha+2, coluna);
+									done = true;
+								}
+							}
+							if(linha > 0 && coluna < SIZE-1){
+								if(tabuleiroMat[linha-1][coluna+1] == '-'){
+									campoMinado.Abrir(linha, coluna+2);
+									done = true;
+								}
+							}
+							if(linha < SIZE-1 && coluna < SIZE-1){
+								if(tabuleiroMat[linha+1][coluna+1] == '-'){
+									campoMinado.Abrir(linha+2, coluna+2);
+									done = true;
+								}
+							}
+							if(linha > 0){
+								if(tabuleiroMat[linha-1][coluna] == '-'){
+									campoMinado.Abrir(linha, coluna+1);
+									done = true;
+								}
+							}
+							if(linha < SIZE-1){
+								if(tabuleiroMat[linha+1][coluna] == '-'){
+									campoMinado.Abrir(linha+2, coluna+1);
+									done = true;
+								}
+							}
+							if(coluna < SIZE-1){
+								if(tabuleiroMat[linha][coluna+1] == '-'){
+									campoMinado.Abrir(linha+1, coluna+2);
+									done = true;
+								}
+							}
+							if(coluna > 0){
+								if(tabuleiroMat[linha][coluna-1] == '-'){
+									campoMinado.Abrir(linha+1, coluna);
+									done = true;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		System.out.println("Final do jogo\n=======");
+		System.out.println(campoMinado.Tabuleiro());
+		System.out.println(campoMinado.JogoStatus().name());
+	}
 
 	//função que converte a string do tabuleiro em uma matriz
 	public char[][] matrizTabuleiro(String tab){
@@ -86,4 +151,111 @@ public class Program {
 		}
 		return contagem;
 	}
+
+	//Conta a quantidade de bombas descobertas no arredores de uma posicao especifica
+	public int contaBombas(int linha, int coluna, char[][] tabuleiroMat){
+		int contagem = 0;
+
+		if(linha > 0){
+			//adjacente acima
+			if(tabuleiroMat[linha -1][coluna] == 'p')
+				contagem++;
+
+			if(coluna > 0){
+				//adjacente superior esquerdo
+				if(tabuleiroMat[linha-1][coluna-1] == 'p')
+					contagem++;
+			}
+			
+			if(coluna < SIZE-1){
+				//adjacente superior direito
+				if(tabuleiroMat[linha-1][coluna+1] == 'p')
+					contagem++;
+			}
+		}
+
+		if(linha < SIZE-1){
+			//adjacente abaixo
+			if(tabuleiroMat[linha +1][coluna] == 'p')
+				contagem++;
+
+			if(coluna > 0){
+				//adjacente inferior esquerdo
+				if(tabuleiroMat[linha+1][coluna-1] == 'p')
+					contagem++;
+			}
+			
+			if(coluna < SIZE-1){
+				//adjacente inferior direito
+				if(tabuleiroMat[linha+1][coluna+1] == 'p')
+					contagem++;
+			}
+		}
+
+		//adjacente esquerdo
+		if(coluna > 0){
+			if(tabuleiroMat[linha][coluna-1]== 'p')
+				contagem++;
+		}
+
+		//adjacente direto
+		if(coluna < SIZE-1){
+			if(tabuleiroMat[linha][coluna+1]== 'p')
+				contagem++;
+		}
+		return contagem;
+	}
+
+	public char[][] colocaBombas (char[][] tabuleiroMat){
+
+		for(int linha = 0; linha < SIZE; linha++){
+			for(int coluna = 0; coluna < SIZE; coluna++){
+				if(contaAbertas(linha, coluna, tabuleiroMat) + contaBombas(linha, coluna, tabuleiroMat) == Character.getNumericValue(tabuleiroMat[linha][coluna])){
+					if(linha > 0 && coluna > 0){
+						if(tabuleiroMat[linha-1][coluna-1] == '-'){
+							tabuleiroMat[linha-1][coluna-1] = 'p';
+						}
+					}
+					if(linha < SIZE-1 && coluna > 0){
+						if(tabuleiroMat[linha+1][coluna-1] == '-'){
+							tabuleiroMat[linha+1][coluna-1] = 'p';
+						}
+					}
+					if(linha > 0 && coluna < SIZE-1){
+						if(tabuleiroMat[linha-1][coluna+1] == '-'){
+							tabuleiroMat[linha-1][coluna+1] = 'p';
+						}
+					}
+					if(linha < SIZE-1 && coluna < SIZE-1){
+						if(tabuleiroMat[linha+1][coluna+1] == '-'){
+							tabuleiroMat[linha+1][coluna+1] = 'p';
+						}
+					}
+					if(linha > 0){
+						if(tabuleiroMat[linha-1][coluna] == '-'){
+							tabuleiroMat[linha-1][coluna] = 'p';
+						}
+					}
+					if(linha < SIZE-1){
+						if(tabuleiroMat[linha+1][coluna] == '-'){
+							tabuleiroMat[linha+1][coluna] = 'p';
+						}
+					}
+					if(coluna < SIZE-1){
+						if(tabuleiroMat[linha][coluna+1] == '-'){
+							tabuleiroMat[linha][coluna+1] = 'p';
+						}
+					}
+					if(coluna > 0){
+						if(tabuleiroMat[linha][coluna-1] == '-'){
+							tabuleiroMat[linha][coluna-1] = 'p';
+						}
+					}
+				}
+			}
+		}
+		return tabuleiroMat;
+	}
+
+	private static final int SIZE = 9;
 }
