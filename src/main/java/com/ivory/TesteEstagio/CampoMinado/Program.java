69ed651ae3307f71f378968f3b2cc9cc2cfbcd4a
musquitinh0 <filipe.silva@ccc.ufcg.edu.br>
2022-03-06 02:21:10 -0300

    Adiciona função que conta quantas casas vazias existem ao redor de uma casa especifica

--- src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
+++ src/main/java/com/ivory/TesteEstagio/CampoMinado/Program.java
@@ -12,19 +12,18 @@ public class Program {
 		
 		// Realize sua codificação a partir deste ponto, boa sorte!
 
-		
 		char[][] tabuleiroMat = new char[SIZE][SIZE];
+		tabuleiroMat =  matrizTabuleiro(campoMinado.Tabuleiro());//Tabuleiro auxiliar no formato de matriz e com as provaveis localizações das bombas dadas por 'p'(pra ficar parecido com a bandeira do jogo de verdade)
+		System.out.println(contaAbertas(4,7,tabuleiroMat));
 		while(campoMinado.JogoStatus().equals(StatusTipo.Aberto)){
-			tabuleiroMat =  matrixTabuleiro(campoMinado.Tabuleiro());
-
 			break;
 		}
 	}
 
 	private static final int SIZE = 9;
 
-
-	public char[][] matrixTabuleiro(String tab){
+	//função que converte a string do tabuleiro em uma matriz
+	public char[][] matrizTabuleiro(String tab){
 		char[][] tabuleiroMat = new char[SIZE][SIZE];
 		for(int i = 0; i < SIZE; i ++){
 			for(int j = 0; j < SIZE; j++){
@@ -32,5 +31,59 @@ public class Program {
 			}
 		}
 		return tabuleiroMat;
+	}
+
+	//Conta a quantidade de casas abertas no arredores de uma posicao especifica
+	public int contaAbertas(int linha, int coluna, char[][] tabuleiroMat){
+		int contagem = 0;
+
+		if(linha > 0){
+			//adjacente acima
+			if(tabuleiroMat[linha -1][coluna] == '-')
+				contagem++;
+
+			if(coluna > 0){
+				//adjacente superior esquerdo
+				if(tabuleiroMat[linha-1][coluna-1] == '-')
+					contagem++;
+			}
+			
+			if(coluna < SIZE-1){
+				//adjacente superior direito
+				if(tabuleiroMat[linha-1][coluna+1] == '-')
+					contagem++;
+			}
+		}
+
+		if(linha < SIZE-1){
+			//adjacente abaixo
+			if(tabuleiroMat[linha +1][coluna] == '-')
+				contagem++;
+
+			if(coluna > 0){
+				//adjacente inferior esquerdo
+				if(tabuleiroMat[linha+1][coluna-1] == '-')
+					contagem++;
+			}
+			
+			if(coluna < SIZE-1){
+				//adjacente inferior direito
+				if(tabuleiroMat[linha+1][coluna+1] == '-')
+					contagem++;
+			}
+		}
+
+		//adjacente esquerdo
+		if(coluna > 0){
+			if(tabuleiroMat[linha][coluna-1]== '-')
+				contagem++;
+		}
+
+		//adjacente direto
+		if(coluna < SIZE-1){
+			if(tabuleiroMat[linha][coluna+1]== '-')
+				contagem++;
+		}
+		return contagem;
 	}
 }
